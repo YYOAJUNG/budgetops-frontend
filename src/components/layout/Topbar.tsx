7734e01731647e4d77029ecdf5de561142051783
yejinj <sitting077@gmail.com>
2025-10-03 00:56:55 +0900

    fix: Topbar.tsx 중복 함수 선언 제거

--- src/components/layout/Topbar.tsx
+++ src/components/layout/Topbar.tsx
@@ -10,15 +10,4 @@ export function Topbar(): JSX.Element {
   )
 }
 
-export function Topbar(): JSX.Element {
-  return (
-    <header className="topbar">
-      <div className="title">멀티 클라우드 리소스 대시보드</div>
-      <div className="actions">
-        <input className="search" placeholder="리소스 검색" />
-      </div>
-    </header>
-  )
-}
-
 
