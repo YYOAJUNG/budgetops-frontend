7b17b509fb035a1f851658a8f3529e12301ad7f5
nAhYUN22 <lily.nahyunlee@gmail.com>
2025-11-06 16:09:15 +0900

    feat: 프론트 백 로그인 기능 연동

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:3001/api',
+    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8080/api',
     NEXT_PUBLIC_USE_MOCK: process.env.NEXT_PUBLIC_USE_MOCK || 'true',
   },
 };
